b44a37528ac8a303e4a10e2653ce896d24647d6d
thesoftasia <thesoftasia@gmail.com>
2022-12-14 16:12:05 +0500

    Sign Up
    Added sign up page

---
+++ signin.php
@@ -0,0 +1,32 @@
+
+<?php include 'includes/header.php';?>
+<section class=" gradient-custom">
+  <div class="container py-5 ">
+    <div class="row d-flex justify-content-center align-items-center">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5 ">
+        <div class="card card-custom-bg text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5">
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h3 class=" mb-2  text-center text-primary">Smart Learning System</h3>
+              <p class="text-dark mb-5 fw-bold text-center">Login</p>
+
+              <div class="form-outline form-white mb-4">
+                <label class="form-label text-dark" for="typeEmailX">Email</label>
+                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+              
+
+              
+                <label class="form-label text-dark" for="typePasswordX">Password</label>
+                <input type="password" id="typePasswordX" class="form-control form-control-lg"/><hr>
+                <p class=""><a class="text-primary" href="#!">Forgot password?</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button class="btn btn-primary btn-lg px-5" type="submit">Login</button></p>
+                <p class="mb-0 text-dark">Don't have an account? <a href="#!" class="text-primary">Register</a></p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ signup.php
@@ -0,0 +1,39 @@
+
+<?php include 'includes/header.php';?>
+<section class=" gradient-custom">
+  <div class="container py-5 ">
+    <div class="row d-flex justify-content-center align-items-center">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5 ">
+        <div class="card card-custom-bg text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5">
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h3 class=" mb-2  text-center text-primary">Smart Learning System</h3>
+              <p class="text-dark mb-5 fw-bold text-center">Register</p>
+
+              <div class="form-outline form-white mb-4">
+                <label class="form-label text-dark" for="typeEmailX">Email</label>
+                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+              
+              
+                <label class="form-label text-dark" for="typeClassX">Class</label>
+                <input type="text" id="typeClassX" class="form-control form-control-lg" />
+              
+              
+                <label class="form-label text-dark" for="typeRollNoX">Roll No</label>
+                <input type="text" id="typeRollNoX" class="form-control form-control-lg" />
+              
+
+              
+                <label class="form-label text-dark" for="typePasswordX">Password</label>
+                <input type="password" id="typePasswordX" class="form-control form-control-lg"/><hr>
+                <button class="btn btn-primary btn-lg px-5" type="submit">Sign Up</button>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
