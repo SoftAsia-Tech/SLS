3246b6ba15d2c3b359e2a5a911c9fcbd8e6f164f
Muhammad Tausif <tausifasia@gmail.com>
2023-01-23 10:15:46 +0500

    schoolID is added to students table
    sls_students table is altered by adding the schoolID to the table.
    
    Co-Authored-By: thesoftasia <77333318+thesoftasia@users.noreply.github.com>

--- master_admin/db_query.sql
+++ master_admin/db_query.sql
@@ -1,2 +1,4 @@
 -- Alter teacher table by adding 'teacher_id'
 ALTER TABLE `sls_classes` ADD `teacher_id` INT NULL AFTER `school_id`;
+-- Alter sls_students table by adding 'schoolID'
+ALTER TABLE `sls_students` ADD `schoolID` INT NOT NULL AFTER `classID`;
