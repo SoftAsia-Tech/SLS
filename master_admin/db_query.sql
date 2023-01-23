29314523e6ceb2a1a8d1105921cfccee9d6740f8
Muhammad Tausif <tausifasia@gmail.com>
2023-01-23 10:44:45 +0500

    Testing of commit.

--- master_admin/db_query.sql
+++ master_admin/db_query.sql
@@ -2,3 +2,4 @@
 ALTER TABLE `sls_classes` ADD `teacher_id` INT NULL AFTER `school_id`;
 -- Alter sls_students table by adding 'schoolID'
 ALTER TABLE `sls_students` ADD `schoolID` INT NOT NULL AFTER `classID`;
+-- sync did not work
